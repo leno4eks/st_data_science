29d430575172578790292b9ff98a8d4479684e15
leno4eks <argunovaarar@gmail.com>
2024-04-22 21:52:03 +0300

    new file:   .vscode/settings.json new file:   __pycache__/game_2v.cpython-311.pyc modified:   game.ipynb modified:   game_2v.py

--- game_2v.py
+++ game_2v.py
@@ -1,18 +1,46 @@
 import numpy as np
 
-number = np.random.randint(1, 101) # загадываем число
-count = 0
+def random_predict(number:int=1) -> int:
+    """Рандомно угадываем число
 
-while True:
-    count += 1
-    predict_number = int(input("Угадай число от 1 до 100"))
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
 
-    if predict_number > number:
-        print("Число должно быть меньше!")
+    Returns:
+        int: Число попыток
+    """
 
-    elif predict_number < number:
-        print("Число должно быть больше!")
+    count = 0
 
-    else:
-        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
-        break # конец игры, выход из цикла
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101) # предполагаемое число
+        if number == predict_number:
+            break # выход из цикла, если угадали
+    return(count)
+
+print(f'Количество попыток: {random_predict()}')
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+
+    count_ls = [] # список для сохранения количества попыток
+    np.random.seed(1) # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls)) # находим среднее количество попыток
+
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return(score)
+if __name__ == '__main__':
+    score_game(random_predict)

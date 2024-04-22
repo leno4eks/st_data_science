a2b3fcbe90d85dfd00d82ead3f865c1a6563f415
leno4eks <argunovaarar@gmail.com>
2024-04-22 20:39:54 +0300

    modified:   game_2v.py

--- game_2v.py
+++ game_2v.py
@@ -1,29 +1,18 @@
-
-
 import numpy as np
-def score_game(random_predict) -> int:
-    
-    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
 
-    Args:
-        random_predict ([type]): функция угадывания
+number = np.random.randint(1, 101) # загадываем число
+count = 0
 
-    Returns:
-        int: среднее количество попыток
-    """
+while True:
+    count += 1
+    predict_number = int(input("Угадай число от 1 до 100"))
 
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(500)) # загадали список чисел
+    if predict_number > number:
+        print("Число должно быть меньше!")
 
-    for number in random_array:
-        count_ls.append(random_predict(number))
+    elif predict_number < number:
+        print("Число должно быть больше!")
 
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
-
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-    return(score)
-
-#RUN
-if __name__ == '__main__':
-    score_game(random_predict)
+    else:
+        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+        break # конец игры, выход из цикла
